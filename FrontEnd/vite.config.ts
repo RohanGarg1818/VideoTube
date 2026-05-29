0f6bde2a58b6aea64434b72ebe6eefb1dead7f9d
Rohan Garg <rohan@Rohans-MacBook-Air.local>
2026-05-29 23:27:03 +0530

    more changes

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://video-tube-fawn.vercel.app/",
         changeOrigin: true,
       },
     },
